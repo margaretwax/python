53db95c72dd4dd99ffde78109cbcd96c518e154d
margaretwax <m.voskresenskaya@MacBook-Pro-el-7.local>
2015-08-25 16:20:16 +0300

    Записанный рекордером сценарий

---
+++ unittest oorraa login.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class unittest oorraa login(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_unittest oorraa login(self):
+        success = True
+        wd = self.wd
+        wd.get("https://front1.test.oorraa.net/")
+        wd.find_element_by_link_text("Login").click()
+        wd.find_element_by_link_text("Login").click()
+        wd.find_element_by_xpath("//section[@class='layout__auth']//label[.='Cell phone']").click()
+        if not wd.find_element_by_id("rs_loginWith_phone").is_selected():
+            wd.find_element_by_id("rs_loginWith_phone").click()
+        wd.find_element_by_name("phone").click()
+        wd.find_element_by_name("phone").clear()
+        wd.find_element_by_name("phone").send_keys("(000) 000-91-92")
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys("123")
+        wd.find_element_by_xpath("//section[@class='layout__auth']//button[.='Sign in']").click()
+        self.assertTrue(success)
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
