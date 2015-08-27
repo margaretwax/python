bb0d5c642fea418ad23c46928f3a82c866103e59
margaretwax <m.voskresenskaya@MacBook-Pro-el-7.local>
2015-08-27 13:13:50 +0300

    Доставка и Логин 2

--- python_training/login2.py
+++ python_training/login2.py
@@ -14,25 +14,54 @@ class login2(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
+
     def test_login2(self):
         success = True
         wd = self.wd
+        self.open_home_page(wd)
+        self.enter_login(wd)
+        self.enter_phone(wd, mobile_phone="(000) 000-91-92")
+        self.enter_password(wd, password="123")
+
+# проба пера дальше кусок
+    def empty_password(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.enter_login(wd)
+        self.enter_phone(wd, mobile_phone="(000) 000-91-92")
+        self.enter_password(wd, password="")
+
+
+    def open_home_page(self, wd):
+        # open home page
         wd.get("https://front1.test.oorraa.net/")
+
+    def enter_login(self, wd):
+        # login
         wd.find_element_by_link_text("Регистрация и вход").click()
         wd.find_element_by_link_text("Вход").click()
         wd.find_element_by_xpath("//label[@for='rs_loginWith_phone']/span/span").click()
         if not wd.find_element_by_id("rs_loginWith_phone").is_selected():
             wd.find_element_by_id("rs_loginWith_phone").click()
+
+    def enter_phone(self, wd, mobile_phone):
+        # enter phone
         wd.find_element_by_name("phone").click()
         wd.find_element_by_name("phone").clear()
-        wd.find_element_by_name("phone").send_keys("(000) 000-91-92")
+        wd.find_element_by_name("phone").send_keys(mobile_phone)
+
+    def enter_password(self, wd, password):
+        # enter password
         wd.find_element_by_name("password").click()
         wd.find_element_by_name("password").clear()
-        wd.find_element_by_name("password").send_keys("123")
+        wd.find_element_by_name("password").send_keys(password)
         wd.find_element_by_xpath("//section[@class='layout__auth']//button[.='Войти']").click()
-        self.assertTrue(success)
-    
+
+
+
+
+
     def tearDown(self):
         self.wd.quit()
 
